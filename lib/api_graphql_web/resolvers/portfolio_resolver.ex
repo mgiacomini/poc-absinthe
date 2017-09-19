2c63f4fef64ab98832bbad0bedc29a52c9c9132e
Mauricio Giacomini Girardello <mauriciogiacomini4@gmail.com>
2017-09-19 16:12:46 -0300

    adding graphql mutation for update existent portfolio

--- lib/api_graphql_web/resolvers/portfolio_resolver.ex
+++ lib/api_graphql_web/resolvers/portfolio_resolver.ex
@@ -6,6 +6,12 @@ defmodule ApiGraphqlWeb.PortfolioResolver do
     {:ok, ApiGraphql.Repo.all(Portfolio)}
   end
 
+  def update(%{id: id, portfolio: portfolio_params}, _info) do
+    Repo.get!(Portfolio, id)
+    |> Portfolio.changeset(portfolio_params)
+    |> Repo.update
+  end
+
   def create(args, _info) do
     %Portfolio{}
     |> Portfolio.changeset(args)
--- lib/api_graphql_web/schema.ex
+++ lib/api_graphql_web/schema.ex
@@ -3,6 +3,10 @@ defmodule ApiGraphqlWeb.Schema do
   alias ApiGraphqlWeb.PortfolioResolver
   import_types ApiGraphqlWeb.Schema.Types
 
+  input_object :portfolio_params do
+    field :name, non_null(:string)
+  end
+
   query do
     field :portfolios, list_of(:portfolio) do
       resolve &PortfolioResolver.all/2
@@ -10,6 +14,13 @@ defmodule ApiGraphqlWeb.Schema do
   end
 
   mutation do
+    field :update_portfolio, type: :portfolio do
+      arg :id, non_null(:integer)
+      arg :portfolio, :portfolio_params
+
+      resolve &PortfolioResolver.update/2
+    end
+
     field :create_portfolio, type: :portfolio do
       arg :name, non_null(:string)
 
